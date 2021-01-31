a2667482bf36fe2483abb2ff3d0d89ac8c97fcab
white-yun <2461710470@qq.com>
2021-01-31 18:54:27 +0800

    项目环境的搭建-路由-antd-sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-
-
+import { BrowserRouter as Router , Link ,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import City from './pages/City'
+import Map from './pages/Map'
+import { Button } from 'antd';
+import './index.scss';
 class App extends React.Component{
     render() {
-        return <div className="app">
-            app
-        </div>
+        return (
+            <Router>
+                <div className="app">
+                    app
+                    <Button type="primary">Button</Button>
+                </div>
+                <ul>
+                    <li><Link to="/home">首页</Link></li>
+                    <li><Link to="/city">城市选择</Link></li>
+                    <li><Link to="/map">地图找房</Link></li>
+                </ul>
+                <Route path="/home" component={Home}></Route>
+                <Route path="/city" component={City}></Route>
+                <Route path="/map" component={Map}></Route>
+            </Router>
+        )
     }
 }
 export default App
---
+++ src/pages/City/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class City extends React.Component{
+    render() {
+        return <div>City 组件</div>
+    }
+}
